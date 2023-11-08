a8bc1fcdc63eb8a6fc6d6ef55c48e3b0b4a01e1f
Rabia Yavuz <rabia.yavuz@d-teknoloji.com.tr>
2023-11-08 14:40:31 +0300

    The project file has been created.

---
+++ LinkVantage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LinkVantage
+//
+//  Created by Rabia Yavuz on 8.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
